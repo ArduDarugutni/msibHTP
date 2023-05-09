6e658cc0402fcd6917e9de9e703092ef6260f7c2
ArduDarugutni <theviel77@gmail.com>
2023-05-09 22:12:33 +0700

    Baru

--- tugas6.sql
+++ tugas6.sql
@@ -1,34 +1,47 @@
--- Membuat table pesanan
-CREATE TABLE pesanan (
-    id_pesanan INT AUTO_INCREMENT PRIMARY KEY,
-    nama_pelanggan VARCHAR(100),
-    total_pembayaran DECIMAL(10, 2),
-    status_pesanan VARCHAR(20)
-);
+-- Tambahkan kolom pembayaran
+MariaDB [dbtoko]> ALTER TABLE pembayaran
+    -> ADD COLUMN status_pembayaran varchar(15) DEFAULT 'belum lunas';
+Query OK, 0 rows affected (0.077 sec)
+Records: 0  Duplicates: 0  Warnings: 0
 
--- Membuat table pembayaran
-CREATE TABLE pembayaran (
-    id_pembayaran INT AUTO_INCREMENT PRIMARY KEY,
-    id_pesanan INT,
-    jumlah_pembayaran DECIMAL(10, 2),
-    tanggal_pembayaran DATE,
-    status_pembayaran VARCHAR(20)
-);
+--TRIGGER
+MariaDB [dbtoko]> DELIMITER $$
+MariaDB [dbtoko]> CREATE TRIGGER set_status_pembayaran BEFORE INSERT ON pembayaran
+    -> FOR EACH ROW
+    -> BEGIN
+    -> DECLARE total_pembayaran DOUBLE;
+    -> DECLARE total_pesanan DOUBLE;
+    -> SELECT SUM(jumlah) INTO total_pembayaran FROM pembayaran WHERE pesanan_id = NEW.pesanan_id;
+    -> SELECT total INTO total_pesanan FROM pesanan WHERE id = NEW.pesanan_id;
+    -> IF total_pembayaran + NEW.jumlah >= total_pesanan THEN
+    -> SET NEW.status_pembayaran = 'lunas';
+    -> END IF;
+    -> END$$
+Query OK, 0 rows affected (0.026 sec)
 
--- Menambahkan kolom status_pembayaran di table pembayaran
-ALTER TABLE pembayaran
-ADD COLUMN status_pembayaran VARCHAR(20);
+MariaDB [dbtoko]> DELIMITER $$
+MariaDB [dbtoko]> CREATE TRIGGER set_status_pembayaran BEFORE INSERT ON pembayaran
+    -> FOR EACH ROW
+    -> BEGIN
+    -> DECLARE total_pembayaran DOUBLE;
+    -> DECLARE total_pesanan DOUBLE;
+    -> SELECT SUM(jumlah) INTO total_pembayaran FROM pembayaran WHERE pesanan_id = NEW.pesanan_id;
+    -> SELECT total INTO total_pesanan FROM pesanan WHERE id = NEW.pesanan_id;
+    -> IF total_pembayaran + NEW.jumlah >= total_pesanan THEN
+    -> SET NEW.status_pembayaran = 'lunas';
+    -> END IF;
+    -> END$$
+Query OK, 0 rows affected (0.028 sec)
 
--- Membuat trigger untuk mengubah status_pembayaran menjadi 'lunas' jika pesanan sudah dibayar
-DELIMITER //
-CREATE TRIGGER update_status_pembayaran
-AFTER INSERT ON pembayaran
-FOR EACH ROW
-BEGIN
-    DECLARE total_pembayaran DECIMAL(10, 2);
-    SELECT total_pembayaran INTO total_pembayaran FROM pesanan WHERE id_pesanan = NEW.id_pesanan;
-    IF NEW.jumlah_pembayaran >= total_pembayaran THEN
-        UPDATE pembayaran SET status_pembayaran = 'lunas' WHERE id_pembayaran = NEW.id_pembayaran;
-    END IF;
-END//
-DELIMITER ;
+MariaDB [dbtoko]> DELIMITER ;
+MariaDB [dbtoko]> INSERT INTO pembayaran (no_kuitansi, tanggal, jumlah, ke, pesanan_id) VALUES
+    ->  ('TV01', '2023-01-25', 2000000, 1, 1);
+Query OK, 1 row affected (0.073 sec)
+
+MariaDB [dbtoko]> select *from pembayaran;
++----+-------------+------------+----------+----+------------+-------------------+
+| id | no_kuitansi | tanggal    |  jumlah  | ke | pesanan_id | status_pembayaran |
++----+-------------+------------+----------+----+------------+-------------------+
+|  1 | TV01        | 2023-01-25 |  4000000 |  1 |          1 |  lunas            |
++----+-------------+------------+----------+----+------------+-------------------+
+1 row in set (0.002 sec)
